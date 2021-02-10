e49b66037f9ee4756eab2aabc74301c400261332
Paul Park <paulpark673@gmail.com>
2021-02-10 14:35:38 -0800

    added image and new color input

--- src/components/ImageForm/ImageForm.js
+++ src/components/ImageForm/ImageForm.js
@@ -1,12 +1,17 @@
 import React from "react";
 import './ImageForm.scss';
+import butterfly from "../../images/butterfly.jpg";
 
 function textInput() {
   return (
     <form className="img-form">
         <div className="img-form__inner">
-            <input type="text" className="img-form__elements img-form__input" placeholder="Please enter text"/>
-            <input type="text" className="img-form__elements img-form__input" placeholder="Please enter text"/>
+            <img className="img-form__elements img-form__image" src={butterfly} />
+            <input type="text" className="img-form__elements img-form__text" placeholder="Please enter text"/>
+            <div className="img-form__blend">
+              <label for="color">Select your blend color: </label>
+              <input className="blend-input" type="color" name="color" value="#ff0000"/>
+            </div>
             <button type="submit" className="img-form__elements img-form__submit">Submit</button>
         </div>
     </form>  
